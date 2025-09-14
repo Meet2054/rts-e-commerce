1a5589af82f4946616ac7a0c2215447f98fc3bdb
akashstwt <akashpatel115347@gmail.com>
2025-09-15 00:09:46 +0530

    tailwind config added

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        sm: "640px",
+        md: "1025px",
+        lg: "1280px",
+      },
+    },
+  },
+  plugins: [],
+};
